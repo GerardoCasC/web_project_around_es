40a4819b240e86ec2a1000deacdfd9145078ae46
Gerardo Casanova C <gerardojccastro@gmail.com>
2026-04-18 13:36:53 -0500

    Proyecto Sprint 6 - Etapa 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-console.log("Mensaje de prueba");
-
 let initialCards = [
   [
     "Valle de Yosemite",
@@ -30,3 +28,54 @@ let initialCards = [
 initialCards.forEach((element) => {
   console.log(element[0]);
 });
+
+const profileSection = document.querySelector(".profile");
+const editProfileButton = profileSection.querySelector(".profile__edit-button");
+const editProfileModal = document.querySelector("#edit-popup");
+const closeEditProfileButton = editProfileModal.querySelector(".popup__close");
+const editProfileForm = editProfileModal.querySelector(".popup__form");
+
+editProfileButton.addEventListener("click", handleOpenEditModal);
+
+closeEditProfileButton.addEventListener("click", () =>
+  closeModal(editProfileModal),
+);
+
+editProfileForm.addEventListener("submit", handleProfileFormSubmit);
+
+function handleOpenEditModal() {
+  openModal(editProfileModal);
+  fillProfileForm();
+}
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+function fillProfileForm() {
+  const nameInput = editProfileForm.querySelector(".popup__input_type_name");
+  const name = profileSection.querySelector(".profile__title");
+  const descriptionInput = editProfileForm.querySelector(
+    ".popup__input_type_description",
+  );
+  const description = profileSection.querySelector(".profile__description");
+  nameInput.value = name.textContent;
+  descriptionInput.value = description.textContent;
+  console.log(name.textContent);
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  const nameInput = editProfileForm.querySelector(".popup__input_type_name");
+  const name = profileSection.querySelector(".profile__title");
+  const descriptionInput = editProfileForm.querySelector(
+    ".popup__input_type_description",
+  );
+  const description = profileSection.querySelector(".profile__description");
+  name.textContent = nameInput.value;
+  description.textContent = descriptionInput.value;
+  closeModal(editProfileModal);
+}
